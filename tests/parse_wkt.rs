6fe50faf536038ef6ee29757290c81371ca86b40
Denis Deniz González <denis2.glez@gmail.com>
2021-11-17 23:06:09 +0100

    Makes tests more concise.

--- tests/parse_wkt.rs
+++ tests/parse_wkt.rs
@@ -1,31 +1,40 @@
-use geos::{Geom, Geometry};
+use geos::{GResult, Geom, Geometry};
 
 #[test]
-fn parse_point() {
+fn parse_point() -> GResult<()> {
     let s = include_str!("data/Point.wkt");
-    let point = Geometry::new_from_wkt(s).expect("Invalid geometry");
+    let point = Geometry::new_from_wkt(s)?;
 
     assert!(point.is_valid());
-    assert!(point.is_simple().unwrap());
-    assert_eq!(point.get_type().unwrap(), "Point");
+    assert!(point.is_simple()?);
+    assert_eq!(point.get_type()?, "Point");
+
+    Ok(())
 }
 
 #[test]
-fn parse_polygon() {
+fn parse_polygon() -> GResult<()> {
     let s = include_str!("data/Polygon1.wkt");
-    let poly = Geometry::new_from_wkt(s).expect("Invalid geometry");
+    let poly = Geometry::new_from_wkt(s)?;
 
     assert!(poly.is_valid());
-    assert!(poly.is_simple().unwrap());
-    assert_eq!(poly.get_type().unwrap(), "Polygon");
+    assert!(poly.is_simple()?);
+    assert_eq!(poly.get_type()?, "Polygon");
+
+    Ok(())
 }
 
 #[test]
-fn parse_geometry_collection() {
+fn parse_geometry_collection() -> GResult<()> {
     let s = include_str!("data/GeometryCollection.wkt");
-    let geo = Geometry::new_from_wkt(s).expect("Invalid geometry");
+    let geo = Geometry::new_from_wkt(s)?;
+
+    let geom2 = Geometry::new_from_wkt("POINT (35 38)").expect("Invalid point");
 
     assert!(geo.is_valid());
-    assert!(geo.is_simple().unwrap());
-    assert_eq!(geo.get_type().unwrap(), "GeometryCollection");
+    assert!(geo.is_simple()?);
+    assert!(geo.contains(&geom2)?);
+    assert_eq!(geo.get_type()?, "GeometryCollection");
+
+    Ok(())
 }
